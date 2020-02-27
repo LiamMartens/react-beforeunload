c6e6e36635f3bce1e32d5866d91fa98121736f4c
Liam Martens <hi@liammartens.com>
2020-02-27 15:19:21 +0100

    Updated with v2

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import babel from 'rollup-plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.tsx',
   output: {
     exports: 'named',
     file: 'lib/index.js',
     format: 'cjs',
   },
-  external: ['prop-types', 'react'],
-  plugins: [babel()],
+  external: ['prop-types', 'react', 'react-router'],
+  plugins: [babel(), typescript()],
 };
---
+++ src/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { matchPath, useHistory } from 'react-router';
+
+export interface IBeforeLeaveOptions {
+  message: string;
+  enableUnload?: boolean;
+  enableRouter?: boolean;
+  path?: string;
+  exact?: boolean;
+}
+
+export const useBeforeleave = (
+  {
+    message,
+    exact = true,
+    path = '',
+    enableUnload = true,
+    enableRouter = true,
+  }: IBeforeLeaveOptions,
+) => {
+  if (enableUnload) {
+    React.useEffect(() => {
+      const onBeforeUnload = (event: BeforeUnloadEvent) => {
+        event.returnValue = message;
+        return message;
+      };
+      window.addEventListener('beforeunload', onBeforeUnload);
+      return () => window.removeEventListener('beforeunload', onBeforeUnload);
+    }, []);
+  }
+
+  if (enableRouter) {
+    const history = useHistory();
+    React.useEffect(() => {
+      const unbind = history.listen(listener => {
+        if (matchPath(listener.pathname, { exact, path: !!path ? path : location.pathname }))
+          history.block(message);
+      });
+      return () => unbind();
+    }, []);
+  }
+}
+
+export const BeforeLeave: React.FunctionComponent<IBeforeLeaveOptions> = (props) => {
+  const { children = null, ...opts } = props;
+  useBeforeleave(opts);
+  if (children) {
+    return (
+      <>
+        {children}
+      </>
+    );
+  }
+  return null;
+};
